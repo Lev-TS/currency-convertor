47ca692c3a402607c72fac3d934de093921b2212
Lev-TS <57505924+Lev-TS@users.noreply.github.com>
2020-08-24 00:17:55 +0300

    adding the fix for the regex expression preventing safar render the app

--- client/src/components/results/results.component.jsx
+++ client/src/components/results/results.component.jsx
@@ -18,7 +18,7 @@ const Results = ({
 			return number
 				.toFixed(2)
 				.toString()
-				.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+				.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 		}
 	};
 
